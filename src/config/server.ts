b7094f98b9fc06323bfffc234533ecb703ce000c
Victor Nevola <victornevolapn@gmail.com>
2021-03-06 22:03:18 -0300

    add server const to jest start tests

--- src/config/server.ts
+++ src/config/server.ts
@@ -2,4 +2,4 @@ import app from './appConfig';
 
 const PORT : string|number = process.env.PORT || 5000;
 
-app.listen(PORT,() => console.log(`hosting @${PORT}`));
+export const server = app.listen(PORT,() => console.log(`hosting @${PORT}`));
