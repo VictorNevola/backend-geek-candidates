318b6f3bce891927af9a269f393c10285c5885c1
Victor Nevola <victornevolapn@gmail.com>
2021-03-11 22:42:59 -0300

    fixing build

--- src/config/appConfig.ts
+++ src/config/appConfig.ts
@@ -1,6 +1,6 @@
 import express, { urlencoded, json } from 'express';
 import cors from 'cors';
-import router from '../routes';
+import router from '../Routes';
 import mongoConnect from './dbConect';
 
 mongoConnect();
